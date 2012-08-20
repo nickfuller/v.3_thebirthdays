de3614a3dc20c6627275d070b9f90a2644ccec55
James Thomason <jamie.thomason@gmail.com>
2012-08-20 17:49:41 -0500

    put routes in config/routes.rb for new, create, index, and show images

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 V3Thebirthdays::Application.routes.draw do
 
   root :to => 'Homepage#bulletin'
-	get '/:size/:color' => 'Homepage#bulletin'
+	get '/:size/:color' => 'Homepage#bulletin', :as => :size_color_homepage
+	get '/images/new' => 'Images#new', :as => :new_image
+	post '/images' => 'Images#create', :as => :images
+	get '/images' => 'Images#index', :as => :images
+	get '/images/:id' => 'Images#show', :as => :image
 	
 end
